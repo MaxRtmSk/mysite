51acbf12affffd235f1ffbb0fd933fefbb2286ef
Max <max.work.dev@gmail.com>
2021-05-21 11:28:26 +0300

    feat: add List

--- src/page/Project.tsx
+++ src/page/Project.tsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import List from '../components/List'
+import { useTypedSelector } from '../hooks/useTypedSelector'
+import { IReact } from '../types/project'
 
 export default function Project() {
+  const { react, reactNative, node } = useTypedSelector(state => state.project)
+
   return (
     <div>
-      Project
+      <div>
+        <h2>React</h2>
+        <List
+          items={react}
+          renderItem={
+            (project: IReact) => <a key={project.id} href={project.gitHub}>{project.title}</a>
+          }
+        />
+      </div>
+      <div>
+        <h2>React Native</h2>
+        <List
+          items={reactNative}
+          renderItem={
+            (project: IReact) => <a key={project.id} href={project.gitHub}>{project.title}</a>
+          }
+        />
+      </div>
+      <div>
+        <h2>Node</h2>
+        <List
+          items={node}
+          renderItem={
+            (project: IReact) => <a key={project.id} href={project.gitHub}>{project.title}</a>
+          }
+        />
+      </div>
     </div>
   )
 }
